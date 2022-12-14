94c77b96d982fea75b779ddac6d085f68ecbcc87
Octavian Rusu <octavian.concept@gmail.com>
2022-12-13 18:38:38 -0700

    Starting to design the API

--- 0.1/app.js
+++ 0.1/app.js
@@ -3,17 +3,47 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+
+/*
+ 
+ Handle:
+ * Logins and Session tokens
+ * GET requests for datasets
+ * Create Pull Requests
+ * Accept Pull Requests and update DB
+
+
+Models:
+	User:
+		* username - str
+		* passward - hash
+		* email    - str
+		* karma?   - int32
+		* Notifications
+		* Messages
+		* Requests
+
+	Pull Request:
+		* Request id - hash
+		* new data   - JSON / CVS
+		* user       - str
+		* status		 - bool + NULL
+*/
+
 app.get('/', (req, res) => {
-	res.send('Landing Page if not logged in \n Notifications page if logged in.')
+	res.send('Hello')	
 })
 
-app.get('/login', (req, res) => {
-	res.send('Login Page')
+app.get('/users/:username', (req, res) => {
+	var username = req.params.username;
+	res.send(`User ${username}`)
 })
 
-app.get('/dataset', (req, res) => {
-	res.send('Load dataset with SLUG or HASH')
+app.get('/datasets/:uuid', (req, res) => {
+	var uuid = req.params.uuid;
+	res.send(`GET Dataset with UUID ${uuid}`)
 })
+
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`)
