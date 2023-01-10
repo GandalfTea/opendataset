1970651f66116ba44ad62256d15d9aa05e1f70a9
Octavian Rusu <octavian.concept@gmail.com>
2023-01-10 00:54:56 -0700

    error catching on tests

--- api/tests/req.py
+++ api/tests/req.py
@@ -8,84 +8,64 @@ import time
 PORT = 3000
 VERBOSE = False
 
+def test(test_name, req_type, req_url, expected_status_code, payload={}):
+    print(f" > {test_name}", end='')
+    API_BASE_URL = f"http://127.0.0.1:{PORT}"
+    st = time.monotonic()
+    try:
+        if req_type=='POST':
+            r = rq.post(f"{API_BASE_URL}{req_url}",
+                    headers={"Content-Type":"application/json"},
+                    data=json.dumps(payload))
+        elif req_type=='GET':
+            r = rq.get(f"{API_BASE_URL}{req_url}")
+        elif req_type=='DELETE':
+            r = rq.delete(f"{API_BASE_URL}{req_url}")
+        else:
+            raise ValueError("URL Request Type not valid.")
+        assert r.status_code == expected_status_code
+
+    except ConnectionError as err:
+        print(err.args)
+    except ValueError as err:
+        print(err.args)
+    except AssertionError as err:
+        padding = (55-(len(test_name)+3))*' '
+        print(f"{padding}FAILED: Status code asserion failed. Expected {expected_status_code} and got {r.status_code}.")
+    except:
+        padding = (55-(len(test_name)+3))*' '
+        print(f"{padding}FAILED. Unknown reason.")
+    else:
+        ed = time.monotonic()
+        et = ed-st
+        padding = (55-(len(test_name)+3))*' '
+        print(f"{padding}{et:7f} seconds.")
+        if VERBOSE: pprint.pprint(r.content.decode())
+
 # CREATE, EDIT and DELETE Users 
 def test_user():
 
     # CREATE
-    print(" > test_user_create:", end='')
     payload = {'username':'demo-user', 'email':'demo-email@example.com', 'password':'demopassword'}
-    st = time.monotonic()
-    r = rq.post(f"http://127.0.0.1:{PORT}/create/user",
-                headers={"Content-Type":"application/json"},
-                data=json.dumps(payload))
-    ed = time.monotonic()
-    assert(r.status_code == 200)
-    et = ed-st
-    print(f"                        {et:7f} seconds.")
-    if VERBOSE:
-        print("Response: ") 
-        pprint.pprint(json.loads(r.content.decode()))
-        print('')
+    test("test_user_creation", "POST", "/create/user", 200, payload)
 
     # LOGIN
-    print(" > test_user_login_wrong_credentials:", end='')
     correct_payload = {'username': 'demo-user', 'password': 'demopassword'}
     wrong_payload   = {'username': 'demo-user', 'password': 'wrongpassword'}
 
-    st = time.monotonic()
-    r = rq.post(f"http://127.0.0.1:{PORT}/login",
-            headers={'content-type': 'application/json'},
-            data=json.dumps(wrong_payload))
-    ed = time.monotonic()
-    assert r.status_code == 400, 'wrong password worked.'
-    et = ed-st
-    print(f"       {et:7f} seconds.")
-
-    print(" > test_user_login_correct_credentials:", end='')
-    st = time.monotonic()
-    r = rq.post(f"http://127.0.0.1:{PORT}/login",
-            headers={'content-type': 'application/json'},
-            data=json.dumps(correct_payload))
-    ed = time.monotonic()
-    assert r.status_code == 200, 'Correct password failed.'
-    et = ed-st
-    print(f"     {et:7f} seconds.")
+    test("test_user_login_wrong_credentials", "POST", "/login", 400, wrong_payload)
+    test("test_user_login_correct_credentials", "POST", "/login", 200, correct_payload)
 
     # EDIT  . . .
-    print(f" > test_user_edit_username           \t    SKIPPED")
-    print(f" > test_user_edit_email              \t    SKIPPED")
-    print(f" > test_user_edit_password           \t    SKIPPED")
+    print(f" > test_user_edit_username                             SKIPPED")
+    print(f" > test_user_edit_email                                SKIPPED")
+    print(f" > test_user_edit_password                             SKIPPED")
 
     # DELETE
-    print(" > test_user_delete:", end='')
-    st = time.monotonic()
-    r = rq.delete(f"http://127.0.0.1:{PORT}/users/{payload['username']}")
-    assert(r.status_code == 200)
-    ed = time.monotonic()
-    et = ed-st
-    print(f"                        {et:7f} seconds.")
-    if VERBOSE: 
-        print("Response: ") 
-        pprint.pprint(json.loads(r.content.decode()))
-        print('')
+    test('test_user_delete', 'DELETE', f"/users/{payload['username']}", 200)
 
 
 # CREATE, EDIT and DELETE Dataset
-
-
-def test_dataset_creation(test_name, payload, expected_status_code):
-    print(f" > {test_name}", end='')
-    st = time.monotonic()
-    r =  rq.post(f"http://127.0.0.1:{PORT}/create/dataset",
-                 headers={"Content-Type":"application/json"},
-                 data=json.dumps(payload))
-    assert(r.status_code == expected_status_code)
-    ed = time.monotonic()
-    et = ed-st
-    padding = (55-(len(test_name)+3))*' '
-    print(f"{padding}{et:7f} seconds.")
-    if VERBOSE: pprint.pprint(r.content.decode())
-
 
 def test_dataset():
     correct_payload = { "name":"demo-dataset", "owner": "GandalfTea", "schema": "Name --> STRING(50)\nName--> STRING(50)", "contributions": "1" };
@@ -96,9 +76,9 @@ def test_dataset():
     #illegal_column_names_payload = { "name":"demo-dataset", "owner": "GandalfTea", "schema": "Name --> STRING(50)\nName--> STRING(50)", "contributions": "2"};
     #database_error_payload = { "name":"demo-dataset", "owner": "GandalfTea", "schema": "Name --> STRING(50)\nName--> STRING(50)", "contributions": "1"};
 
-    test_dataset_creation("test_dataset_creation_correct_payload", correct_payload, 201)
-    test_dataset_creation("test_dataset_creation_owner_not_existant", user_not_existant_payload, 404)
-    test_dataset_creation("test_dataset_creation_ds_already_exists", dataset_already_exists_payload, 409)
+    test("test_dataset_creation_correct_payload",    'POST', '/create/dataset', 201, correct_payload)
+    test("test_dataset_creation_owner_not_existant", 'POST', '/create/dataset', 404, user_not_existant_payload)
+    test("test_dataset_creation_ds_already_exists",  'POST', '/create/dataset', 409, dataset_already_exists_payload)
 
     # Require Demo Data
     #test_dataset_creation("test_dataset_creation_schama_generation_failure", schema_generation_failure_payload, 421)
@@ -126,6 +106,12 @@ def test_dataset():
     # DELETE 
     print(" > test_database_deletion                              SKIPPED")
 
+
+def test_dataset_frontend():
+    ds_id = 'demo-dataset'
+    test("test_dataset_frontend", 'GET', f"/dataset/{ds_id}/details", 200)
+    
+
 if __name__ == '__main__':
     for arg in sys.argv[1:]:
         if 'PORT' in arg:
@@ -138,3 +124,5 @@ if __name__ == '__main__':
     test_user()
     print('\n')
     test_dataset()
+    print('\n')
+    test_dataset_frontend()
