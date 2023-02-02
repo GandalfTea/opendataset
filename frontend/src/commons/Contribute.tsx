3b24e7f680247ae86573feb9947e0b3a0b4338e1
Octavian Rusu <octavian.concept@gmail.com>
2023-02-02 13:06:08 -0700

    contribution page: get file metadata and display, add buttons

--- frontend/src/commons/Contribute.tsx
+++ frontend/src/commons/Contribute.tsx
@@ -20,6 +20,9 @@ class DropCSV extends React.Component {
     super(props);
     this.state = { drop: false, loading: false };
     this.csv_data = "";
+		this.filename = "";
+		this.filesize = "";
+		this.filentries = 0;
     this.drop_handler = this.drop_handler.bind(this);
     this.drag_over_handler = this.drag_over_handler.bind(this);
   }
@@ -31,10 +34,13 @@ class DropCSV extends React.Component {
       [...e.dataTransfer.items].forEach((f, i) => {
         if (f.kind === "file") {
           const file = f.getAsFile();
+					this.filename = file.name;
+					this.filesize = file.size;
           var reader = new FileReader();
           reader.readAsText(file, "UTF-8");
           reader.onload = (e) => {
             this.csv_data = e.target.result.split("\n");
+						this.filentries = this.csv_data.length-1;
             this.setState({ loading: false, drop: true });
           };
           reader.onerror = (e) => {
@@ -60,11 +66,26 @@ class DropCSV extends React.Component {
     }
   }
 
+	display_filesize(size: number) {
+		if     (size > 1e12) return (size*1e-12).toPrecision(4).toString() + " TB"
+		else if (size > 1e9) return (size*1e-9) .toPrecision(4).toString() + " GB"
+		else if (size > 1e6) return (size*1e-6) .toPrecision(4).toString() + " MB"
+		else if (size > 1e3) return (size*1e-3) .toPrecision(4).toString() + " KB"
+		else if (size < 1e3) return size.toString() + " B"
+	}
+
   render() {
     if (this.state.drop) {
       return (
-        <div className="file-contents card">
-          {this.csv_format_handler(this.csv_data)}
+        <div className="file-description card">
+					<h4>{this.filename}, {this.display_filesize(this.filesize)}, {this.filentries} entries</h4>
+					<div className="file-contents">
+          	{this.csv_format_handler(this.csv_data)}
+					</div>
+					<div>
+						<button className="button-empty-black"><p>Next</p></button>
+						<button className="button-full-black"><p>Done</p></button>
+					</div>
         </div>
       );
     } else {
