54afa0323bb926b20df08b6973c1922b15116f85
Octavian Rusu <octavian.concept@gmail.com>
2023-02-26 12:20:44 -0700

    update tests only pass if the demo ds is not deleted

--- api/tests/test_dataset.py
+++ api/tests/test_dataset.py
@@ -1,8 +1,10 @@
 
 import unittest
-from utils import *
+from setup import *
 
 class TestDataset(unittest.TestCase):
+
+    # Create
 
     def test_dataset_creation_invalid_owner(self):
         name = 'demo-dataset'
@@ -91,6 +93,9 @@ class TestDataset(unittest.TestCase):
         r = rq.delete(f"{BASE_API_URL}/ds/{name}")
         # check if deleted
 
+
+    # File Upload
+
     def test_dataset_creation_file_upload_csv(self):
         pass
     def test_dataset_creation_file_upload_invalid_csv(self):
@@ -98,12 +103,97 @@ class TestDataset(unittest.TestCase):
     def test_dataset_creation_file_upload_json(self):
         pass
     def test_dataset_creation_file_upload_invalid_json(self):
+        pass    
+
+    def test_dataset_creation_schema_generation_failure(self):
+        pass
+    def test_dataset_creation_command_generation_failure(self):
+        pass
+    def test_dataset_creation_illegal_column_names(self):
+        pass
+    def test_dataset_creation_database_error(self):
         pass
 
-    def test_dataset_deletion(self):
+
+    # Edit
+
+    def test_update_description(self):
         name = 'demo-dataset'
-        r = rq.delete(f"{BASE_API_URL}/ds/{name}")
-        self.assertEqual(r.status_code, 204)
+        new_description = "Updated Description"
+        r = rq.patch(f"{BASE_API_URL}/ds/{name}/details?q=description",
+                     headers={"Content-Type": "application/json"},
+                     data = json.dumps({"data": new_description}))
+        self.assertEqual(r.status_code, 200)
+        r = rq.get(f"{BASE_API_URL}/ds/{name}/details?q=description")
+        r = r.json()
+        self.assertEqual(r['description'], new_description)
+
+    def test_update_readme(self):
+        name = 'demo-dataset'
+        new_readme = "Updated Readme"
+        r = rq.patch(f"{BASE_API_URL}/ds/{name}/details?q=readme",
+                     headers={"Content-Type": "application/json"},
+                     data = json.dumps({"data": new_readme}))
+        self.assertEqual(r.status_code, 200)
+        r = rq.get(f"{BASE_API_URL}/ds/{name}/details?q=readme")
+        r = r.json()
+        self.assertEqual(r['readme'], new_readme)
+
+    def test_update_licence(self):
+        name = 'demo-dataset'
+        new_licence = 0 
+        r = rq.patch(f"{BASE_API_URL}/ds/{name}/details?q=licence",
+                     headers={"Content-Type": "application/json"},
+                     data = json.dumps({"data": new_licence}))
+        self.assertEqual(r.status_code, 200)
+        r = rq.get(f"{BASE_API_URL}/ds/{name}/details?q=licence")
+        r = r.json()
+        self.assertEqual(r['licence'], new_licence)
+
+    def test_update_guidelines(self):
+        name = 'demo-dataset'
+        new_guidelines = "Updated Guidelines"
+        r = rq.patch(f"{BASE_API_URL}/ds/{name}/details?q=contribution_guidelines",
+                     headers={"Content-Type": "application/json"},
+                     data = json.dumps({"data": new_guidelines}))
+        self.assertEqual(r.status_code, 200)
+        r = rq.get(f"{BASE_API_URL}/ds/{name}/details?q=contribution_guidelines")
+        r = r.json()
+        self.assertEqual(r['contribution_guidelines'], new_guidelines)
+
+    def test_update_contibutors_number(self):
+        name = 'demo-dataset'
+        new_cont = 42069 
+        r = rq.patch(f"{BASE_API_URL}/ds/{name}/details?q=num_contributors",
+                     headers={"Content-Type": "application/json"},
+                     data = json.dumps({"data": new_cont}))
+        self.assertEqual(r.status_code, 200)
+        r = rq.get(f"{BASE_API_URL}/ds/{name}/details?q=contribution_guidelines")
+        r = r.json()
+        print(r)
+        self.assertEqual(r['contribution_guidelines'], new_cont)
+    
+    def test_update_num_entries(self):
+        name = 'demo-dataset'
+        new_nument = 42069 
+        r = rq.patch(f"{BASE_API_URL}/ds/{name}/details?q=num_entries",
+                     headers={"Content-Type": "application/json"},
+                     data = json.dumps({"data": new_nument}))
+        self.assertEqual(r.status_code, 200)
+        r = rq.get(f"{BASE_API_URL}/ds/{name}/details?q=num_entries")
+        r = r.json()
+        self.assertEqual(r['contribution_guidelines'], new_nument)
+
+
+    # Delete
+
+    def test_dataset_deletion(self):
+
+        #TODO: This causes prev tests to fail
+
+        name = 'demo-dataset'
+        #r = rq.delete(f"{BASE_API_URL}/ds/{name}")
+        #self.assertEqual(r.status_code, 204)
         # Test if really deleted
 
 if __name__ == "__main__":
